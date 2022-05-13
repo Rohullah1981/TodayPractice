4aca48f6e3f421cd5383829db73202c1e449208a
Rohullah1981 <105240715+Rohullah1981@users.noreply.github.com>
2022-05-13 17:06:26 -0400

    We add value to Strings

--- src/newprojectpractice/GroupStudy.java
+++ src/newprojectpractice/GroupStudy.java
@@ -3,7 +3,8 @@ package newprojectpractice;
 public class GroupStudy {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String b = "Ahmad";
+		String d =  "Rohullah";
 
 	}
 
